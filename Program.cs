40f5d3c0d7a52d5147e21599d207528b29c42344
ayushsrivastava7619 <shivamsrivastava23112001@gmail.com>
2023-09-05 20:50:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment37
+{
+    class BaseClass
+    {
+        private int xy;
+
+
+        public BaseClass(int initialValue)
+        {
+            xy = initialValue;
+        }
+
+
+        public void sprivtepro(int newValue)
+        {
+            xy = newValue;
+        }
+
+
+        public int privtepro()
+        {
+            return xy;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main()
+        {
+
+            BaseClass myObject = new BaseClass(42);
+
+
+            Console.WriteLine("Initial private property value: " + myObject.privtepro());
+
+            myObject.sprivtepro(100);
+
+
+            Console.WriteLine("Updated private property value: " + myObject.privtepro());
+            Console.Read();
+        }
+    }
+}
